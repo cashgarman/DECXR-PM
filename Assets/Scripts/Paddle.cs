d85f0904262b4aa82ee3765cda22071546719cba
Cash Garman <cashgarman@hotmail.co.uk>
2021-01-12 20:19:21 -0800

    Added new PhysicsPickup component to physical grabbing/throwing of objects. Started work on VRPong.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    private void Update()
+    {
+        // Calculate the target position the paddle should move to
+        var targetPosition = new Vector3(
+            target.position.x,      // Target's X coordinate
+            target.position.y,      // Target's Y coordinate
+            transform.position.z);  // The paddle's Z coordinate
+
+        // Calculate the direction the paddle needs to move to reach the target
+        var direction = Vector3.Normalize(targetPosition - transform.position);
+
+        // Move the paddle towards its target
+        transform.position += direction * speed * Time.deltaTime;
+
+        // TODO: Add this to prevent the jittering
+        // If we're close enough to the target
+            // Just move to the target
+    }
+}
---
+++ Assets/Scripts/PhysicsPickup.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsPickup : Pickup
+{
+    private FixedJoint joint;
+
+    private Queue<Vector3> previousVelocities = new Queue<Vector3>();
+    private Vector3 prevPosition;
+    public float throwBoost;
+    public int numVelocitySamples;
+
+    internal override void OnPickup(Grabber grabber)
+    {
+        base.OnPickup(grabber);
+
+        // Create a fixed joint between the object and the grabber
+        joint = gameObject.AddComponent<FixedJoint>();
+        joint.connectedBody = grabber.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Calculate the velocity of the object based on how far it moved in a frame
+        var velocity = transform.position - prevPosition;
+
+        // Remember the position this frame, so we can use it to calculate the velocity in the next frame
+        prevPosition = transform.position;
+
+        // Add this calculated velocity to the list of previous velocities
+        previousVelocities.Enqueue(velocity);
+
+        // Make sure we stay within the limit of previous velocities
+        if(previousVelocities.Count > numVelocitySamples)
+        {
+            previousVelocities.Dequeue();
+        }
+    }
+
+    internal override void OnDrop()
+    {
+        base.OnDrop();
+
+        // Destroy the fixed joint
+        Destroy(joint);
+
+        // Calculate the smoothed velocity
+        Vector3 smoothedVelocity = Vector3.zero;
+        foreach(var previousVelocity in previousVelocities)
+        {
+            smoothedVelocity += previousVelocity;
+        }
+        smoothedVelocity /= previousVelocities.Count;
+
+        // Apply the calculated velocity to the rigid body to throw the object
+        GetComponent<Rigidbody>().velocity = smoothedVelocity * throwBoost;
+    }
+}
